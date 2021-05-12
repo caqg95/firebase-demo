0d08c54b178939459c7c98645ba78fdbbe8de767
Carlos Quintero Grande <cquinterogrande@gmail.com>
2021-05-12 14:39:29 -0600

    Upload

---
+++ src/components/Signup.js
@@ -0,0 +1,61 @@
+import React, { useRef, useState } from "react";
+import { Form, Button, Card, Alert } from "react-bootstrap";
+import { useAuth } from "../contexts/AuthContext";
+
+const Signup = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+  const { signup } = useAuth();
+  const [error, setError] = useState("");
+  const [Loading, setLoading] = useState(false);
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("Las contraseñas no coinciden");
+    }
+    try {
+      setError("");
+      setLoading(true);
+      signup(emailRef.current.value, passwordRef.current.value);
+    } catch {
+      setError("Algo ha fallado al crear el usuario");
+    }
+    setLoading(false);
+  }
+  return (
+    <div>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Registrarse</h2>
+          {/* {currentUser && currentUser.email} */}
+          {/* {JSON.stringify(currentUser)} */}
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" ref={emailRef} required />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Contraseña</Form.Label>
+              <Form.Control type="password" ref={passwordRef} required />
+            </Form.Group>
+            <Form.Group id="password-confirm">
+              <Form.Label>Confirmar Contraseña</Form.Label>
+              <Form.Control type="password" ref={passwordConfirmRef} required />
+            </Form.Group>
+            <Button disabled={Loading} className="mt-2 w-100" type="submit">
+              Registrarse
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        Ya tienes una cuenta? Iniciar Sesion
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
